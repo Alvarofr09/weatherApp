8b0ce9c58b707c43e8f7af0270fa12316ffe9ea9
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2023-12-12 23:15:48 +0100

    console log

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ async function checkWeather(city) {
   } else {
     let data = await response.json();
 
+    console.log(data);
+
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°C";
